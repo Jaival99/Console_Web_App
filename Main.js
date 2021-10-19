9b55e7bf73ea3736d65670e8e141b315b4c11ccf
Jaival99 <83907557+Jaival99@users.noreply.github.com>
2021-10-19 19:47:45 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,14 @@
+console.log("My First Console Project")
+var first_name="Jaival";
+var last_name="Manghnani";
+console.log(first_name.concat(last_name));
+var a=10;
+var b=5;
+var add= a + b;
+console.log(add)
+var subtract= a - b;
+console.log(subtract)
+var multiply= a * b;
+console.log(multiply)
+var divide= a / b;
+console.log(divide)
